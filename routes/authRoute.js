75e7bd39625aa60eb69e427a1ad1e8d9a373019e
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-06-13 18:39:08 +0530

    defafwef

---
+++ routes/authRoute.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { loginController, registerController, testController } from '../controller/authController.js'
+import { requireSignIn, isAdmin } from '../middlewares/authMiddleware.js'
+//router object
+
+const router = express.Router()
+
+//routing
+
+//Register
+router.post('/register', registerController)
+
+//Login
+router.post('/login', loginController)
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+export default router
