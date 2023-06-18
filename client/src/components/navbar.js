279c846e1e4c10df8fdf5fd76a7545e9d489cbf1
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-06-18 11:04:24 +0530

    navbar dropdown

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,10 +3,12 @@ import { AiOutlineClose, AiOutlineMenu, AiOutlineShoppingCart } from 'react-icon
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../context/auth";
 import { toast, Toaster } from "react-hot-toast";
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 const Navbar = () => {
-    const [drop, setDrop] = useState(false)
-    const init = true
+
     const [auth, setAuth] = useAuth()
     const [toggle, setToggle] = useState(false);
     const handleLogout = () => {
@@ -56,16 +58,37 @@ const Navbar = () => {
                             ) : (
                                 <>
                                     <li>
-                                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between  text-white hover:text-teal-400">{auth?.user.name} <svg className="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg></button>
-                                        <div id="dropdownNavbar" className="overflow-hidden z-20 hidden font-normal rounded-lg shadow w-44 bg-gray-700 divide-gray-600">
+                                        <Menu as="div" className="relative inline-block text-left">
+                                            <div>
+                                                <Menu.Button className="inline-flex w-full justify-center rounded-md text-white hover:text-teal-400">
+                                                    {auth?.user.name}
+                                                    <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                </Menu.Button>
+                                            </div>
 
-                                            <div className="">
-                                                <NavLink href="#" to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`} className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Dashboard</NavLink>
-                                            </div>
-                                            <div className="">
-                                                <NavLink href="#" onClick={handleLogout} to="/login" className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Sign out</NavLink>
-                                            </div>
-                                        </div>
+                                            <Transition
+                                                as={Fragment}
+                                                enter="transition ease-out duration-100"
+                                                enterFrom="transform opacity-0 scale-95"
+                                                enterTo="transform opacity-100  scale-100"
+                                                leave="transition ease-in duration-75"
+                                                leaveFrom="transform opacity-100 scale-100"
+                                                leaveTo="transform opacity-0 scale-95 "
+                                            >
+                                                <Menu.Items className="absolute overflow-hidden right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                    <div className=" ">
+                                                        <Menu.Item>
+                                                            <NavLink href="#" to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`}
+                                                                className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Dashboard</NavLink>
+                                                        </Menu.Item>
+                                                        <Menu.Item>
+                                                            <NavLink href="#" onClick={handleLogout} to="/login"
+                                                                className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Sign out</NavLink>
+                                                        </Menu.Item>
+                                                    </div>
+                                                </Menu.Items>
+                                            </Transition>
+                                        </Menu>
                                     </li>
                                 </>
                             )
@@ -102,23 +125,45 @@ const Navbar = () => {
                                             <NavLink to="/register">Register</NavLink>
                                         </li>
                                         <li className="hover:text-teal-400 text-white">
-                                            <NavLink to="/login">Login</NavLink>
+                                            <NavLink to="/login">Login </NavLink>
                                         </li>
 
                                     </>
                                 ) : (
                                     <>
                                         <li>
-                                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between  text-white hover:text-teal-400">{auth?.user.name} <svg className="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg></button>
-                                            <div id="dropdownNavbar" className="overflow-hidden z-20 hidden font-normal rounded-lg shadow w-44 bg-gray-700 divide-gray-600">
 
-                                                <div className="">
-                                                    <NavLink href="#" to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`} className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Dashboard</NavLink>
+                                            <Menu as="div" className="relative inline-block text-left">
+                                                <div>
+                                                    <Menu.Button className="inline-flex w-full justify-center rounded-md text-white hover:text-teal-400">
+                                                        {auth?.user.name}
+                                                        <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                    </Menu.Button>
                                                 </div>
-                                                <div className="">
-                                                    <NavLink href="#" onClick={handleLogout} to="/login" className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Sign out</NavLink>
-                                                </div>
-                                            </div>
+
+                                                <Transition
+                                                    as={Fragment}
+                                                    enter="transition ease-out duration-100"
+                                                    enterFrom="transform opacity-0 scale-95"
+                                                    enterTo="transform opacity-100  scale-100"
+                                                    leave="transition ease-in duration-75"
+                                                    leaveFrom="transform opacity-100 scale-100"
+                                                    leaveTo="transform opacity-0 scale-95 "
+                                                >
+                                                    <Menu.Items className="absolute overflow-hidden right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                        <div className=" ">
+                                                            <Menu.Item>
+                                                                <NavLink href="#" to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`}
+                                                                    className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Dashboard</NavLink>
+                                                            </Menu.Item>
+                                                            <Menu.Item>
+                                                                <NavLink href="#" onClick={handleLogout} to="/login"
+                                                                    className="block px-4 py-2 text-sm hover:bg-gray-600 hover:text-teal-400 text-white">Sign out</NavLink>
+                                                            </Menu.Item>
+                                                        </div>
+                                                    </Menu.Items>
+                                                </Transition>
+                                            </Menu>
                                         </li>
 
                                     </>
