3f01060fcf42287bcfe6392b00c7c032015b9ab2
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-06-20 02:52:46 +0530

    minor design changes

--- client/src/components/adminMenu.js
+++ client/src/components/adminMenu.js
@@ -5,7 +5,6 @@ const AdminMenu = () => {
     return (
         <div>
 
-
             <div className="w-full sm:w-50  border rounded-lg bg-gray-500 border-gray-600 text-white">
                 <NavLink to="/dashboard/admin/create-category" className="block w-full px-4 py-2 text-white border rounded-t-lg  bg-gray-700 hover:bg-gray-600 border-gray-600">
                     Create Category
