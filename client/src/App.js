1d25c67549c54a93fa1d962b18e438c028f08f79
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-06-21 18:13:39 +0530

    csss updates and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-
 import Footer from './components/footer'
 import Navbar from './components/navbar';
 import Home from './pages/home';
