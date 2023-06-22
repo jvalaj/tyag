62baf6e0fd15abb0325b54c1deeac8c9c750e528
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-06-22 19:57:43 +0530

    design changes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -78,36 +78,53 @@ const Home = () => {
                 <div className="min-h-[80vh] w-full sm:w-auto up-2 flex-col flex justify-center">
 
                     <div className="w-full min-h-[70vh] bg-gray-300 rounded-lg mt-3  p-2">
-                        <p className='pt-1 block text-center'>All Products</p>
-                        <div className='flex gap-2 m-1 flex-wrap'>
-                            {products.map((p) => (
+                        <p className='pt-2 block text-center'>All Products</p>
+                        <div className="md:grid md:grid-cols-[60%_40%] p-2">
+                            <div className='flex flex-col gap-2 m-1'>
+                                {products.map((p) => (
 
-                                <div key={p._id} className="w-[9rem] sm:w-[13rem] grid grid-rows-2 min-h-[14rem] sm:min-h-[14rem] border rounded-lg shadow bg-gray-800 border-gray-700">
+                                    <div key={p._id} className="mb-3 flex flex-row sm:grid sm:grid-cols-[30%_70%] justify-between h-[13rem] sm:h-[10rem]  border rounded-lg shadow bg-gray-800 border-gray-700">
 
-                                    <div className='bg-orange-500 row-span-1 flex justify-center bg-white  rounded-t-lg overflow-hidden pb-2'>
-                                        <img className=" object-contain h-auto mx-auto w-full  rounded-t-lg" src={`/api/v1/product/product-photo/${p._id}`} alt="photo" />
+                                        <div className='w-full flex justify-center bg-white rounded-lg overflow-hidden pb-2'>
+                                            <img className=" object-contain  h-auto w-full " src={`/api/v1/product/product-photo/${p._id}`} alt="photo" />
 
+                                        </div>
+
+                                        <div className="w-full  flex flex-col justify-between p-2 gap-1 ">
+                                            <div className="flex flex-col gap-1">
+                                                <div className='py-2'>
+                                                    <h5 className="my-auto text-md font-bold tracking-tight text-white">{p.name}</h5>
+
+                                                </div>
+                                                <div className=' py-2'>
+                                                    <p className="my-auto text-sm leading-none text-gray-400"> {p.description.substring(1, 120)}...</p>
+
+                                                </div>
+                                            </div>
+
+
+
+                                            <div className=' text-right flex flex-row leading-none tracking-tighter'>
+                                                <div className="w-full text-lg  flex">
+                                                    <p className="my-auto text-left font-bold text-green-500">Rs. {p.price}</p>
+
+                                                </div>
+                                                <div className="w-full text-xs text-right">
+                                                    <button className=" tracking-tighter  bg-blue-600 text-xs p-2 rounded-full text-white">Add to Cart</button>
+
+                                                </div>
+
+                                            </div>
+
+                                        </div>
                                     </div>
 
-                                    <div className="bg-yellow-500 row-span-1 gap-2 grid px-2 pt-2 grid-rows-3  ">
-                                        <div className='bg-white row-span-1 mt-2'>
-                                            <h5 className=" text-md font-bold tracking-tight text-white">{p.name}</h5>
+                                ))}
 
-                                        </div>
-                                        <div className='bg-violet-500 pt-2 row-span-1 '>
-                                            <p className="text-sm leading-none text-gray-400"> {p.description.substring(0, 60)}...</p>
-                                            <p className=" text-sm text-right my-auto text-green-500">Rs. {p.price}</p>
-                                        </div>
-                                        <div className='bg-red-500 row-span-1 pt-2  sm:pt-3'>
-                                            <button className="bg-blue-500 text-xs p-2 rounded-lg text-white">Add to Cart</button>
-
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                            ))}
-
+                            </div>
+                            <div className="hidden w-full min-v-[40vh] text-center sm:block">
+                                Cart
+                            </div>
                         </div>
                     </div>
                     <div className="m-2 flex justify-center p-3">
