477e14192ef89b698606d112b7b2b05b8090dfa0
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-07-12 23:08:12 +0530

    login

--- client/src/pages/Auth/login.js
+++ client/src/pages/Auth/login.js
@@ -48,35 +48,35 @@ const Login = () => {
             <section className="min-h-[80vh] bg-white flex items-center justify-center">
                 <div className="w-full md:w-[60%] flex flex-col my-auto items-center justify-center px-6 py-8 mx-auto ">
 
-                    <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+                    <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-white border-gray-300">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl block text-center font-bold leading-tight tracking-tight md:text-2xl text-white">
+                            <h1 className="text-xl block text-center font-bold leading-tight tracking-tight md:text-2xl text-">
                                 Login to your Account
                             </h1>
                             <form className="space-y-4 md:space-y-5" onSubmit={handleSubmit} >
 
                                 <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-">Your email</label>
                                     <input value={email}
                                         onChange={(e) => setEmail(e.target.value)}
                                         type=""
                                         name=""
                                         id=""
-                                        className=" sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border border-gray-600 placeholder-gray-400 text-white "
+                                        className=" sm:text-sm rounded-lg block w-full p-2.5 bg-gray-300 border border-gray-600 placeholder-gray-400 text- "
                                         placeholder="name@company.com"
                                         required />
                                 </div>
 
 
                                 <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-">Password</label>
                                     <input value={password}
                                         onChange={(e) => setPassword(e.target.value)}
                                         type=""
                                         name=""
                                         id=""
                                         placeholder="••••••••"
-                                        className=" border  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white "
+                                        className=" border  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-300 border-gray-600 placeholder-gray-400 text- "
                                         required />
                                     <p className="text-sm block text-right m-1 font-light text-gray-400">
                                         <button onClick={() => { navigate('/forgot-password') }} className="hover:underline text-primary-500">
@@ -87,10 +87,10 @@ const Login = () => {
 
 
 
-                                <button type="submit" className="w-full text-white bg-gray-800 hover:bg-gray-700 transition shadow-xl font-medium rounded-lg text-sm px-5 py-2.5 text-center border border-gray-400">Login</button>
+                                <button type="submit" className="w-full text- bg-white hover:bg-gray-300 transition shadow-xl font-medium rounded-lg text-sm px-5 py-2.5 text-center border border-gray-400">Login</button>
 
 
-                                <p className="text-sm block text-center font-light text-gray-400">
+                                <p className="text-sm block text-center font-light text-gray-600">
                                     Don't have an account? <Link to="/register" className="font-medium hover:underline text-primary-500">Register here</Link>
                                 </p>
                             </form>
