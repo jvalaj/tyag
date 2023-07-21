27b3cbcf53ec3b74e28a5eaf5d478651e06e569b
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2023-07-21 17:14:59 +0530

    items list design

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -91,10 +91,10 @@ const CategoryProduct = () => {
               <div className='flex flex-wrap  gap-2 m-1'>
                 {products.map((p) => (
 
-                  <div key={p._id} className="mb-3 w-[9rem] sm:w-[10rem] lg:w-[13rem] h-[10rem] sm:h-[12rem] lg:h-[16rem] grid grid-rows-[40%_60%]   border rounded-lg shadow bg-gray-800 border-gray-700">
+                  <div key={p._id} className="mb-3 w-[9rem] sm:w-[10rem] lg:w-[13rem] h-[10.5rem] sm:h-[12rem] lg:h-[16rem] grid grid-rows-[40%_60%] lg:grid-rows-[50%_50%] shadow border rounded-lg border-blue-200 bg-gradient-to-b from-blue-200 to-sky-100  ">
 
                     <div onClick={() => navigate(`/product/${p.slug}`)}
-                      target="_blank" className='w-full cursor-pointer hover:opacity-40 px-auto flex justify-center bg-white rounded-lg overflow-hidden pb-2'>
+                      target="_blank" className='w-full rounded-lg cursor-pointer hover:opacity-40 px-auto flex justify-center bg-white  overflow-hidden pb-2'>
 
 
                       <img className=" object-contain  mx-auto h-auto w-full " src={`/api/v1/product/product-photo/${p._id}`} alt="photo" />
@@ -102,27 +102,31 @@ const CategoryProduct = () => {
 
                     </div>
 
-                    <div className="w-full  flex flex-col px-2 ">
+                    <div className="w-full flex flex-col px-2 ">
                       <div className="flex justify-center flex-col gap-1">
                         <div className='pt-2'>
-                          <h5 className="my-auto text-md sm:text-lg font-semibold tracking-tight text-white">{p.name}</h5>
+                          <h5 className="my-auto text-md sm:text-lg font-semibold tracking-tight text-gray-700">{p.name}</h5>
                         </div>
                         <div className='hidden lg:inline m-0'>
-                          <p className="my-auto text-sm leading-none text-gray-400"> {p.description.substring(0, 50)}...</p>
+                          <p className="my-auto text-sm leading-none text-gray-500"> {p.description.substring(0, 50)}...</p>
+
+                        </div>
+                        <div className='lg:hidden m-0'>
+                          <p className="my-auto !text-xs leading-none text-gray-500"> {p.description.substring(0, 30)}...</p>
 
                         </div>
                       </div>
 
 
 
-                      <div className='flex flex-col mt-auto mb-3 lg:flex-row'>
-                        <div className="w-full  text-sm sm:text-lg">
-                          <p className="my-auto text-white">Rs. {p.price}</p>
+                      <div className='flex mt-auto mb-2 flex-row'>
+                        <div className="w-full my-auto flex text-md sm:text-lg">
+                          <p className="my-auto font-semibold text-gray-700">Rs. {p.price}</p>
 
                         </div>
                         <div className="w-full text-right m-0 text-xs">
 
-                          <button className=" hover:opacity-60 bg-blue-600 text-xs p-2 m-0 rounded-full text-white"
+                          <button className="text-white hover:opacity-60 bg-sky-400 shadow-xl text-xs p-2 m-0 rounded-full "
                             onClick={() => {
                               let myCart = [...cart]
                               const index = myCart.findIndex(item => item._id === p._id);
@@ -147,8 +151,8 @@ const CategoryProduct = () => {
                               }
                             }}
                           >
-                            <p className="hidden sm:inline">Add to Cart</p>
-                            <p className="sm:hidden m-0 px-1 py-0">+</p>
+                            <p className="hidden !text-xs  lg:inline ">Add to Cart</p>
+                            <p className="lg:hidden m-0 px-1 py-0 ">+</p>
 
 
 
@@ -160,6 +164,7 @@ const CategoryProduct = () => {
 
                     </div>
                   </div>
+
 
 
 
@@ -180,9 +185,9 @@ const CategoryProduct = () => {
                   : ``}
 
               </div>
-              <div className="hidden w-full min-v-[40vh] text-center md:block">
+              <div className="hidden bg-gradient-to-b from-blue-500 to-sky-200 rounded-lg p-2 w-full min-v-[40vh] text-center md:block">
                 {cart?.map((p) => (
-                  <div key={p._id} className="mt-2 p-2 flex flex-row overflow-hidden justify-between h-[4rem]  border rounded-lg bg-gray-200 shadow">
+                  <div key={p._id} className="mt-2 p-2 flex flex-row overflow-hidden justify-between h-[4rem]  border rounded-lg bg-gray-100 shadow-xl">
 
                     <div className="w-[30rem] flex gap-1">
                       <h5 className="my-auto text-left text-md font-bold ">{p.name}</h5>
@@ -212,9 +217,9 @@ const CategoryProduct = () => {
                 ))}
                 {cart?.length ? <>
                   <div className="w-full justify-center mt-2 text-lg flex">
-                    <button className="my-auto font-bold p-2 rounded-lg w-full border border-green-800" onClick={() => navigate("/cart")}>Checkout</button>
+                    <button className="my-auto mt-4 font-bold p-2 py-3 rounded-lg w-full transition  hover:opacity-70 backdrop-blur-md bg-white/30 shadow-xl" onClick={() => navigate("/cart")}>Checkout</button>
                   </div></> : <>
-                  <div className="p-2 min-h-[20vh] flex justify-center items-center bg-gray-200 rounded-lg">
+                  <div className="p-2 h-full flex justify-center items-center backdrop-blur-xl bg-white/30 rounded-lg">
                     <p className="block text-center">Your Cart Is Empty</p>
                   </div>
                 </>}
